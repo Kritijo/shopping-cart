73cadc5afccacbc1f4d865f68aa4e95641c9a639
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2025-03-25 18:30:10 +0530

    feat: fetch product data and pass to Card component

--- src/components/shop-component/Shop.jsx
+++ src/components/shop-component/Shop.jsx
@@ -1,10 +1,28 @@
 import NavBar from "../navbar-component/NavBar";
+import { useState, useEffect } from "react";
+import fetchProducts from "./fetchProducts";
+import Card from "../card-component/Card";
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const productArr = await fetchProducts();
+            setProducts(productArr);
+        };
+        fetchData();
+    }, []);
+
     return (
-        <>
-            <NavBar></NavBar>
-        </>
+        <main>
+            <NavBar />
+            <section className="products-grid">
+                {products.map((product) => (
+                    <Card key={product.id} product={product} />
+                ))}
+            </section>
+        </main>
     );
 };
 
