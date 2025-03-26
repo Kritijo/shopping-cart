780a6a7b5160d92c1eccd4f90c45bc650e91bbf3
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2025-03-26 10:35:55 +0530

    feat: add input for quantity selection

--- src/components/card-component/Card.jsx
+++ src/components/card-component/Card.jsx
@@ -4,12 +4,24 @@ const Card = ({ product }) => {
     return (
         <div className={styles.productCard}>
             <div>
-                <img src={product.image} className={styles.img}/>
+                <img src={product.image} className={styles.img} />
             </div>
             <h3>{product.title}</h3>
             <h3>${product.price}</h3>
             <p className={styles.desc}>{product.description}</p>
-            <button className={styles.button}>Add to cart</button>
+            <div className={styles.bttnInputDiv}>
+                <button className={styles.button}>Add to cart</button>
+                <div className={styles.inputDiv}>
+                    <label htmlFor={`quantity-${product.id}`}>Qty:</label>
+                    <input
+                        type="number"
+                        min={0}
+                        id={`quantity-${product.id}`}
+                        placeholder="1"
+                        className={styles.input}
+                    ></input>
+                </div>
+            </div>
         </div>
     );
 };
